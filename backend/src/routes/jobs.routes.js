26643af9a5258b7f6e4416c2a6891e739543003b
Keyvan Hojabr <keyvanhojabr@gmail.com>
2026-06-23 11:08:04 +0200

    add middleware

--- backend/src/routes/jobs.routes.js
+++ backend/src/routes/jobs.routes.js
@@ -1,11 +1,43 @@
 import express from "express";
 
+import authMiddleware
+from "../middleware/auth.middleware.js";
+
+import {
+
+  getJobs,
+
+  createJob,
+
+}
+
+from "../controllers/jobs.controller.js";
+
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    message: "Jobs route works ✅",
-  });
-});
+
+
+
+router.use(authMiddleware);
+
+
+router.get(
+
+  "/",
+
+  getJobs
+
+);
+
+
+router.post(
+
+  "/",
+
+  createJob
+
+);
+
 
 export default router;
