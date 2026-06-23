4020a03c9a6a67078a69ed393f19907069fadb2a
Keyvan Hojabr <keyvanhojabr@gmail.com>
2026-06-23 10:40:03 +0200

    handle register with aut

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,90 @@
+import bcrypt from "bcryptjs";
+
+import jwt from "jsonwebtoken";
+
+import User from "../models/User.js";
+
+
+
+
+export const register = async (req, res) => {
+
+  try {
+
+    const { name, email, password } = req.body;
+
+
+    const userExists = await User.findOne({
+      email,
+    });
+
+    if (userExists) {
+
+      return res.status(400).json({
+        message: "User already exists",
+      });
+
+    }
+
+
+    const hashedPassword =
+      await bcrypt.hash(password, 10);
+
+
+    const user = await User.create({
+
+      name,
+
+      email,
+
+      password: hashedPassword,
+
+    });
+
+
+    const token = jwt.sign(
+
+      {
+        userId: user._id,
+      },
+
+      process.env.JWT_SECRET,
+
+      {
+        expiresIn: "7d",
+      }
+
+    );
+
+
+    res.status(201).json({
+
+      token,
+
+      user: {
+
+        id: user._id,
+
+        name: user.name,
+
+        email: user.email,
+
+      },
+
+    });
+
+  }
+
+  catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+
+      message: "Server Error",
+
+    });
+
+  }
+
+};
