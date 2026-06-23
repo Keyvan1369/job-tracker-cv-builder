2d6eeb69f2bde5202f5a7865e96f05ddf0f048c4
Keyvan Hojabr <keyvanhojabr@gmail.com>
2026-06-23 14:34:53 +0200

    to give other param from back

--- frontend/src/context/authContext.jsx
+++ frontend/src/context/authContext.jsx
@@ -3,31 +3,101 @@ import { createContext, useContext, useState, useEffect } from "react";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+
   const [user, setUser] = useState(null);
 
+
   useEffect(() => {
+
     const token = localStorage.getItem("token");
 
-    if (token) {
-      setUser({ token });
+    const userData = localStorage.getItem("user");
+
+
+    if (token && userData) {
+
+      setUser({
+
+        token,
+
+        ...JSON.parse(userData),
+
+      });
+
     }
+
   }, []);
 
+
+
   const login = (data) => {
-    localStorage.setItem("token", data.token);
-    setUser({ token: data.token });
+
+    localStorage.setItem(
+
+      "token",
+
+      data.token
+
+    );
+
+
+    localStorage.setItem(
+
+      "user",
+
+      JSON.stringify(data.user)
+
+    );
+
+
+    setUser({
+
+      token: data.token,
+
+      ...data.user,
+
+    });
+
   };
+
+
 
   const logout = () => {
+
     localStorage.removeItem("token");
+
+    localStorage.removeItem("user");
+
     setUser(null);
+
   };
 
+
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+
+    <AuthContext.Provider
+
+      value={{
+
+        user,
+
+        login,
+
+        logout,
+
+      }}
+
+    >
+
       {children}
+
     </AuthContext.Provider>
+
   );
+
 };
 
-export const useAuth = () => useContext(AuthContext);
+
+export const useAuth = () =>
+
+  useContext(AuthContext);
