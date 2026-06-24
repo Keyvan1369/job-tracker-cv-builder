327f7df500bf582d36735128bc9a49455eacdd7e
Keyvan Hojabr <keyvanhojabr@gmail.com>
2026-06-24 17:39:09 +0200

    make cv_model

---
+++ backend/src/models/cv.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const cvSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    fullName: String,
+    email: String,
+    phone: String,
+    location: String,
+    summary: String,
+    skills: String,
+
+    experiences: [
+      {
+        company: String,
+        position: String,
+        period: String,
+        description: String,
+      },
+    ],
+
+    educations: [
+      {
+        school: String,
+        degree: String,
+        year: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model(
+  "CV",
+  cvSchema
+);
