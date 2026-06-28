434b81eb79024e4b84eb22faab6ef6ea905a646c
Keyvan Hojabr <keyvanhojabr@gmail.com>
2026-06-28 23:29:54 +0200

    create StateCard

---
+++ frontend/src/components/dashboard/StatCard.jsx
@@ -0,0 +1,65 @@
+import { Card, CardContent, Typography, Box } from "@mui/material";
+
+export default function StatCard({
+  title,
+  value,
+  icon,
+  color = "primary",
+}) {
+  return (
+    <Card
+      elevation={2}
+      sx={{
+        borderRadius: 3,
+        transition: "0.3s",
+
+        "&:hover": {
+          transform: "translateY(-6px)",
+          boxShadow: 8,
+        },
+      }}
+    >
+      <CardContent>
+
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+
+          <Box>
+
+            <Typography
+              color="text.secondary"
+              gutterBottom
+            >
+              {title}
+            </Typography>
+
+            <Typography
+              variant="h4"
+              fontWeight="bold"
+            >
+              {value}
+            </Typography>
+
+          </Box>
+
+          <Box
+            sx={{
+              bgcolor: `${color}.main`,
+              color: "white",
+              p: 2,
+              borderRadius: 2,
+              display: "flex",
+            }}
+          >
+            {icon}
+          </Box>
+
+        </Box>
+
+      </CardContent>
+    </Card>
+  );
+}
