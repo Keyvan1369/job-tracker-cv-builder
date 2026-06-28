a60a4c9478cb55a7d6f51426deaa42425613d311
Keyvan Hojabr <keyvanhojabr@gmail.com>
2026-06-28 23:23:22 +0200

    add proxy to access localhost

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: [
       'job-tracker-cv-builder-1.onrender.com'
-    ]
-  }
+    ],
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
