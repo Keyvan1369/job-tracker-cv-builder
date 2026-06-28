2b1ec136a32f744ca5c92b5282d76b9a5c587607
Keyvan Hojabr <keyvanhojabr@gmail.com>
2026-06-28 22:57:42 +0200

    create Topbar

---
+++ frontend/src/components/layout/Topbar.jsx
@@ -0,0 +1,77 @@
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  IconButton,
+  Avatar,
+  Badge,
+} from "@mui/material";
+
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import SearchIcon from "@mui/icons-material/Search";
+
+import { useAuth } from "../../context/AuthContext";
+
+export default function Topbar() {
+
+  const { user } = useAuth();
+
+  return (
+
+    <AppBar
+      position="fixed"
+      elevation={0}
+      color="inherit"
+      sx={{
+        width: "calc(100% - 260px)",
+        ml: "260px",
+        borderBottom: "1px solid #eee",
+      }}
+    >
+
+      <Toolbar>
+
+        <Typography
+          variant="h5"
+          fontWeight="bold"
+        >
+          Dashboard
+        </Typography>
+
+        <Box sx={{ flexGrow: 1 }} />
+
+        <IconButton>
+
+          <SearchIcon />
+
+        </IconButton>
+
+        <IconButton>
+
+          <Badge
+            badgeContent={3}
+            color="error"
+          >
+
+            <NotificationsIcon />
+
+          </Badge>
+
+        </IconButton>
+
+        <Avatar
+          sx={{
+            ml: 2,
+          }}
+        >
+          {user?.name?.charAt(0)}
+        </Avatar>
+
+      </Toolbar>
+
+    </AppBar>
+
+  );
+
+}
