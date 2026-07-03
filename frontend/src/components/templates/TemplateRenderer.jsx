ada86174639a8f3da24131e54f6eb44ef297db92
Keyvan Hojabr <keyvanhojabr@gmail.com>
2026-07-03 16:33:49 +0200

    add to temp_render

--- frontend/src/components/templates/TemplateRenderer.jsx
+++ frontend/src/components/templates/TemplateRenderer.jsx
@@ -4,6 +4,7 @@ import ProfessionalTemplate from "./ProfessionalTemplate.jsx";
 import CreativeTemplate from "./CreativeTemplate.jsx";
 import ExecutiveTemplate from "./ExecutiveTemplate.jsx";
 import SpectrumPulseTemplate from "./SpectrumPulseTemplate.jsx";
+import DeveloperTemplate from "./DeveloperTemplate";
 
 export default function TemplateRenderer({
     template,
@@ -25,6 +26,9 @@ export default function TemplateRenderer({
 
         case "SpectrumPulse":
             return <SpectrumPulseTemplate data={data} />;
+
+        case "developer":
+            return <DeveloperTemplate data={data} />;
 
         default:
             return <ModernTemplate data={data}/>;
