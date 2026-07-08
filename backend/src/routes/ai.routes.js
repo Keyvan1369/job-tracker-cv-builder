a90525d19b5f5f013e8411f18f3da07c75e44583
Keyvan Hojabr <keyvanhojabr@gmail.com>
2026-07-08 15:02:32 +0200

    fix import

--- backend/src/routes/ai.routes.js
+++ backend/src/routes/ai.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { aiEngine } from "../controllers/ai.controller.js";
-import { protect } from "../middleware/auth.middleware.js";
+import  protect  from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
