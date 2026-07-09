2c0f77a8939ed6853aeb8027fa5b3783b20c4f51
Keyvan Hojabr <keyvanhojabr@gmail.com>
2026-07-09 10:14:48 +0200

    create AI_loding

---
+++ frontend/src/components/ai/AILoading.jsx
@@ -0,0 +1,19 @@
+import "./AILoading.css";
+
+export default function AILoading(){
+
+    return(
+
+        <div className="ai-loading">
+
+            <div className="loader"/>
+
+            <h2>Analyzing your resume...</h2>
+
+            <p>Please wait a few seconds.</p>
+
+        </div>
+
+    )
+
+}
